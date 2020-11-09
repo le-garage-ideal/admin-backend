5fb4cab53a1a82e7ec960dc3ca2b82d45eb4596a
Benjamin Caure <bcaure@gmail.com>
2020-11-09 11:35:02 +0100

    :bug: login API multiple send on login error

--- routes/login.route.js
+++ routes/login.route.js
@@ -6,18 +6,17 @@ const loginRouter = express.Router();
 loginRouter.post('', (req, res) => {
   if (!req.body.username || !backendUsers.has(req.body.username)) {
     res.status(401).send({ errorMessage: 'Unknown username / password' });
-  }
-  if (!req.body.password || backendUsers.get(req.body.username) !== req.body.password) {
+  } else if (!req.body.password || backendUsers.get(req.body.username) !== req.body.password) {
     res.status(401).send({ errorMessage: 'Unknown username / password' });
+  } else {
+    const data = {
+      name: req.body.username,
+    };
+    const expiration = '6h';
+
+    const token = jwt.sign({ data, }, jwtSecret, { expiresIn: expiration, algorithm: 'HS256' });
+    res.status(200).json({ username: req.body.username, token });
   }
-
-  const data = {
-    name: req.body.username,
-  };
-  const expiration = '6h';
-
-  const token = jwt.sign({ data, }, jwtSecret, { expiresIn: expiration, algorithm: 'HS256' });
-  res.status(200).json({ username: req.body.username, token });
 });
 
 exports.loginRouter = loginRouter;
