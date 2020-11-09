f490fc62ae7993045698f26948f2a96ecdecb299
Benjamin Caure <bcaure@gmail.com>
2020-11-09 11:34:36 +0100

    :sparkles: add frontend bundle for heroku

---
+++ public/precache-manifest.e1b6b4c6e2ec4a11e2c4d80247689709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2415f9ebaaeada8b2de045239100b723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cad81a1ee2aa2cf92f1c",
+    "url": "/static/css/main.5776a144.chunk.css"
+  },
+  {
+    "revision": "0b52797f87ffff17c473",
+    "url": "/static/js/2.1d605c99.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.1d605c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad81a1ee2aa2cf92f1c",
+    "url": "/static/js/main.0c056bb6.chunk.js"
+  },
+  {
+    "revision": "c7a7b49bef804c41c0b6",
+    "url": "/static/js/runtime-main.29514d1a.js"
+  }
+]);
